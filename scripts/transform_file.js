tinn2015/fabric.js

commit af1aa0339446d18a56fd73ee3fabb6e97050c408

fix NS var usage

scripts/transform_file.js
@@ -12,16 +12,16 @@ function readFile(file) {
     return fs.readFileSync(path.resolve(wd, file)).toString('utf-8');;
 }
 
-function getVariableNameOfKey(raw, key) {
-    const regex = new RegExp(`(.+)=\\s*${key.replaceAll('.', '\\.')}`, 'm');
-    const result = regex.exec(raw);
-    return result && result[1].trim();
-}
-
 function getVariableNameOfNS(raw, namespace) {
-    const regex = new RegExp(`(.+)=\\s*${namespace.replaceAll('.', '\\.')}\\.+$`, 'm');
+    const regex = new RegExp(`\\s*(.+)=\\s*${namespace.replaceAll('.', '\\.')}`, 'gm');
     const result = regex.exec(raw);
     return result ? result[1].trim() : namespace;
+}
+
+function getNSFromVariableName(raw, varname) {
+    const regex = new RegExp(`\\s*${varname}\\s*=\\s*(.*)\\s*?,\\s*`, 'gm');
+    const result = regex.exec(raw);
+    return result ? result[1].trim() : null;
 }
 
 function findObject(raw, charStart, charEnd, startFrom = 0) {
@@ -110,9 +110,15 @@ function findClassBase(raw, regex) {
     const result = regex.exec(raw);
     if (!result) throw new Error('FAILED TO PARSE');
     const [match, classNSRaw, superClassRaw] = result;
-    const namespace = classNSRaw.trim();
+    const [first, ...rest] = classNSRaw.trim().split('.');
+    const namespace = [getNSFromVariableName(raw, first), ...rest].join('.');
     const name = namespace.slice(namespace.lastIndexOf('.') + 1);
-    const superClasses = superClassRaw?.trim().split(',').filter(raw => !raw.match(/\/\*+/) && raw).map(key => key.trim()) || [];
+    const superClasses = superClassRaw?.trim().split(',')
+        .filter(raw => !raw.match(/\/\*+/) && raw).map(key => key.trim())
+        .map(val => {
+            const [first, ...rest] = val.split('.');
+            return [getNSFromVariableName(raw, first), ...rest].join('.');
+        }) || [];
     const rawObject = findObject(raw, '{', '}', result.index);
     const NS = namespace.slice(0, namespace.lastIndexOf('.'));
     const klass = fabric.util.resolveNamespace(NS === 'fabric' ? null : NS)[name];
@@ -302,7 +308,8 @@ function convertFile(type, source, dest) {
         console.log({
             state: 'failure',
             type,
-            source: path.relative(wd, source)
+            source: path.relative(wd, source),
+            error
         });
         error.file = file;
         return error;
@@ -330,13 +337,14 @@ function convert(options = {}) {
         failed.push(...errors);
         createIndex && generateIndexFile(dir, files, ext);
     }
+    
     classDirs.forEach(klsDir => {
         const dir = path.resolve(srcDir, klsDir);
         const result = fs.readdirSync(dir).map(file => {
             return convertFile('class', path.resolve(dir, file), overwriteExisitingFiles ? false : name => path.resolve(dir, `${name}.${ext}`));
         });
         finalize(dir, result);
-    })
+    });
 
     const mixinFiles = fs.readdirSync(mixinsDir).map(file => {
         return convertFile('mixin', path.resolve(mixinsDir, file), overwriteExisitingFiles ? false : path.resolve(mixinsDir, `${getMixinName(file)}.${ext}`));

commit 98e3e1222eca5d5fa81cd50dffc7a10ebf8c5fdf

fix "fix NS var usage"
This fixes  a regression made by commit af1aa0339446d18a56fd73ee3fabb6e97050c408.

scripts/transform_file.js
@@ -13,13 +13,16 @@ function readFile(file) {
 }
 
 function getVariableNameOfNS(raw, namespace) {
-    const regex = new RegExp(`\\s*(.+)=\\s*${namespace.replaceAll('.', '\\.')}`, 'gm');
+    const regex = new RegExp(`\\s*(.+)=\\s*${namespace.replaceAll('.', '\\.')}[^(]+`, 'm');
     const result = regex.exec(raw);
+    console.log(regex.source, namespace,result && result[1].trim())
     return result ? result[1].trim() : namespace;
 }
 
 function getNSFromVariableName(raw, varname) {
+    if (varname === 'fabric') return 'fabric';
     const regex = new RegExp(`\\s*${varname}\\s*=\\s*(.*)\\s*?,\\s*`, 'gm');
+    console.log(regex.source, varname)
     const result = regex.exec(raw);
     return result ? result[1].trim() : null;
 }
@@ -356,7 +359,7 @@ function convert(options = {}) {
         return convertFile('class', path.resolve(srcDir, file), overwriteExisitingFiles ? false : name => path.resolve(srcDir, `${name}.${ext}`));
     });
     finalize(mixinsDir, additionalFiles);
-
+    return;
     console.error(`failed files:`);
     failed.map(console.error)
 }